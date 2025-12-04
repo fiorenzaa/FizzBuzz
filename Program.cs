080a1d5693ab981fd6e44901807b2a9f3d64997a
fiorenzaa <fiorenza.laila24@gmail.com>
2025-12-04 14:26:16 +0700

    Membuat program dan running

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for (int i = 1; i <=100; i++)
+  if (i % 3 == 0)
+    Console.WriteLine("Fizz");
+  else if (i % 5 == 0)
+    Console.WriteLine("Buzz");
+  else if (i % 15 == 0)
+    Console.WriteLine("FizzBuzz");
+  else
+    Console.WriteLine(i);
